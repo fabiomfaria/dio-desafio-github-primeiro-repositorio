09baecdf42f4424a5f0ca075503ab9b69fdc230f
fabiomfaria <fabiomfaria@gmail.com>
2022-01-11 16:43:14 -0300

    correção do código escrito no vídeo da aula de Eclipse para poder rodar.

--- Bootcamp-Cognizant-Java-Developer/a-Dominando-IDEs-Java/primeiroProgramaUsandoIDEAula/src/br/com/dio/PrimeiroProgramaIDEAula.java
+++ Bootcamp-Cognizant-Java-Developer/a-Dominando-IDEs-Java/primeiroProgramaUsandoIDEAula/src/br/com/dio/PrimeiroProgramaIDEAula.java
@@ -3,38 +3,50 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroProgramaIDEAula {
+    public static void main(String[] args) {
+        Gato gato = new Gato(null, null, null);
+        System.out.println(gato);
 
-	public static void main(String[] args) {
-		Gato gato = new Gato(null, null, null);
-		//Livro livro = new Livro();
+        Livro livro = new Livro("CorelDRAW por Fábio Faria",287);
+        System.out.println(livro);
 
-		System.out.println(gato);
+/*      int a = 5;
+        int b = 2;
+        System.out.println("Hello Devs! " + (a+b));*/
+        
+    }
+}
 
-	}
+class Livro{
+    private String nome;
+    private Integer numPaginas;
 
-	class Livro {
-		private String nome;
-		private String npag;
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
 
-		public Livro(String nome, String npag) {
-			this.setNome(nome);
-			this.setNpag(npag);
-		}
+    public String getNome() {
+        return nome;
+    }
 
-		public String getNome() {
-			return nome;
-		}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
 
-		public String getNpag() {
-			return npag;
-		}
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
 
-		public void setNpag(String npag) {
-			this.npag = npag;
-		}
-	}
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
