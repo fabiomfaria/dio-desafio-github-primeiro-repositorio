e1521d2db2aefde296c9f10230651d2b4c952438
fabiomfaria <fabiomfaria@gmail.com>
2022-01-11 16:34:03 -0300

    upgrade do código daaula do Intellij

--- Bootcamp-Cognizant-Java-Developer/a-Dominando-IDEs-Java/src/br/com/dio/PrimeiroProgramaIntellij.java
+++ Bootcamp-Cognizant-Java-Developer/a-Dominando-IDEs-Java/src/br/com/dio/PrimeiroProgramaIntellij.java
@@ -5,12 +5,47 @@ import br.com.dio.model.Gato;
 public class PrimeiroProgramaIntellij {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
 
+        Livro livro = new Livro("CorelDRAW por Fábio Faria",287);
+        System.out.println(livro);
 
 /*      int a = 5;
         int b = 2;
         System.out.println("Hello Devs! " + (a+b));*/
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
