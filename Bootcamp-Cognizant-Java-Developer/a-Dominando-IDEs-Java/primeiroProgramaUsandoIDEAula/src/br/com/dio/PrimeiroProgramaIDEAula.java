831c82498dea7459dd2d7babc4c95aeb3264800a
fabiomfaria <fabiomfaria@gmail.com>
2022-01-11 10:18:51 -0300

    Exemplos de Aula, codigo escrito pela professora

---
+++ Bootcamp-Cognizant-Java-Developer/a-Dominando-IDEs-Java/primeiroProgramaUsandoIDEAula/src/br/com/dio/PrimeiroProgramaIDEAula.java
@@ -0,0 +1,38 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramaIDEAula {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato(null, null, null);
+		//Livro livro = new Livro();
+		
+		System.out.println(gato);
+		
+
+	}
+	
+class Livro{
+	private String nome;
+	private String npag;
+	
+	public Livro(String nome, String npag) {
+		this.setNome(nome);
+		this.setNpag(npag);
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNpag() {
+		return npag;
+	}
+	public void setNpag(String npag) {
+		this.npag = npag;
+	}
+}
+
+}
