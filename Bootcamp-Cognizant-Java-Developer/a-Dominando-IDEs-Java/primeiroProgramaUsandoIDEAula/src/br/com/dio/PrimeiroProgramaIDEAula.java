c97ed95fd406ea7b482a250acb1b112475e4c81d
fabiomfaria <fabiomfaria@gmail.com>
2022-01-11 14:57:59 -0300

    upload do código da aula Degub no Eclipse

--- Bootcamp-Cognizant-Java-Developer/a-Dominando-IDEs-Java/primeiroProgramaUsandoIDEAula/src/br/com/dio/PrimeiroProgramaIDEAula.java
+++ Bootcamp-Cognizant-Java-Developer/a-Dominando-IDEs-Java/primeiroProgramaUsandoIDEAula/src/br/com/dio/PrimeiroProgramaIDEAula.java
@@ -7,32 +7,34 @@ public class PrimeiroProgramaIDEAula {
 	public static void main(String[] args) {
 		Gato gato = new Gato(null, null, null);
 		//Livro livro = new Livro();
-		
+
 		System.out.println(gato);
-		
 
 	}
-	
-class Livro{
-	private String nome;
-	private String npag;
-	
-	public Livro(String nome, String npag) {
-		this.setNome(nome);
-		this.setNpag(npag);
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getNpag() {
-		return npag;
-	}
-	public void setNpag(String npag) {
-		this.npag = npag;
-	}
-}
 
+	class Livro {
+		private String nome;
+		private String npag;
+
+		public Livro(String nome, String npag) {
+			this.setNome(nome);
+			this.setNpag(npag);
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getNpag() {
+			return npag;
+		}
+
+		public void setNpag(String npag) {
+			this.npag = npag;
+		}
+	}
 }
