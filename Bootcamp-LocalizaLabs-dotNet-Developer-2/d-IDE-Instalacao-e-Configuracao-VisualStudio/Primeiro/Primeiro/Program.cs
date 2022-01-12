659a35f1f6c3da4530a1df4698a727c6e500a9ab
fabiomfaria <fabiomfaria@gmail.com>
2022-01-12 10:58:51 -0300

    adicionando arquivos de aula de C#

---
+++ Bootcamp-LocalizaLabs-dotNet-Developer-2/d-IDE-Instalacao-e-Configuracao-VisualStudio/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "hello";
+            var b = "DEVs!";
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
