82ef7702c819cac062d2fc0c65c4aa92e65307e6
fabiomfaria <fabiomfaria@gmail.com>
2022-02-04 22:51:33 -0300

    updated array

--- Bootcamp-LocalizaLabs-dotNet-Developer-2/j-Fundamentos-de-Colecoes-e-LINQ-com-dotNET/Colecoes/Program.cs
+++ Bootcamp-LocalizaLabs-dotNet-Developer-2/j-Fundamentos-de-Colecoes-e-LINQ-com-dotNET/Colecoes/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] arrayInteiros = new int[3]; 
+           
+           arrayInteiros[0] = 10;
+           arrayInteiros[1] = 20;
+           arrayInteiros[2] = 30;
+
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                System.Console.WriteLine($"[{i}] = " + arrayInteiros[i]);
+            }
+
+            System.Console.WriteLine("---------------------");
+
+            foreach (var item in arrayInteiros)
+            {
+               System.Console.WriteLine(item); 
+            }
+        }
+    }
+}
